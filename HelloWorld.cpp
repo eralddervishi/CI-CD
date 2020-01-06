bb21cfa676cafaf3ce443f90098da2b90931320f
Erald Dervishi <erald.dervishi001@umb.edu>
2020-01-06 11:59:21 -0500

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+/*Author: Erald Dervishi
+*/
+#include<iostream>
+
+//Single line comment
+using namespace std;
+
+//This is where the execution of program begins
+int main()
+{
+   // displays Hello World! on screen
+   cout<<"Hello World!";
+
+   return 0;
+}
