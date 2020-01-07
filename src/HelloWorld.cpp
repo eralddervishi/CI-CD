ecbc75b18b220de6376229f24935735674287f63
EraldDervishi <54956229+eralddervishi@users.noreply.github.com>
2020-01-07 18:10:04 -0500

    Add files via upload

---
+++ src/HelloWorld.cpp
@@ -0,0 +1,16 @@
+/*Author: Erald Dervishi
+*/
+#include<iostream>
+
+//Single line comment
+using namespace std;
+
+//This is where the execution of program begins
+int main(int argc, char *argv[])
+{
+   // displays Hello World! on screen
+   std::cout << "Hello World!" << std::endl;
+   return 0;
+}
+
+
